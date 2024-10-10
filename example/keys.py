7b5f5929408e74fc0362fb35eef73456c0371a03
Adam Miller <miller@adammiller.io>
2024-10-09 21:45:08 -0400

    keyslib: add tmux plugin, start README

--- example/keys.py
+++ example/keys.py
@@ -2,3 +2,4 @@
 # ruff: noqa: F401, F811
 
 from keys.htop import __name__
+from keys.tmux import __name__
--- tmux/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Key binding options and defaults
-
-default_completion_key="Space"
-completion_key=$(tmux show-option -gqv "@keys_completion_key")
-completion_key=${logging_key:-$default_completion_key}
